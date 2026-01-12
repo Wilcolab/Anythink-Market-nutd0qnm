f9f5b3d891a0885b459c0c65c55dd9eab22e0fd6
Abinash Sharma <83633679+abinasharma001@users.noreply.github.com>
2026-01-12 10:51:37 +0000

    Format and finalize context-aware dot.case function

--- refined_prompt.js
+++ refined_prompt.js
@@ -4,13 +4,26 @@ function toCamelCase(str) {
   }
   
   return str
-    .replace(/[-_\s]+/g, ' ')  // Replace separators with spaces
+    .replace(/[-_\s]+/g, ' ')
     .split(' ')
-    .filter(word => word.length > 0)  // Remove empty words
+    .filter(word => word.length > 0)
     .map((word, index) => 
       index === 0 
         ? word.toLowerCase() 
         : word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
     )
     .join('');
+}
+
+function toDotCase(str) {
+  if (typeof str !== 'string' || str === null || str === undefined) {
+    throw new Error('Input must be a non-null, non-undefined string');
+  }
+  
+  return str
+    .replace(/[-_\s]+/g, ' ')
+    .split(' ')
+    .filter(word => word.length > 0)
+    .map(word => word.toLowerCase())
+    .join('.');
 }
